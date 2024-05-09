dea834ea2d04378229d5667213eecabd994d8fff
juditmaria <jumoar@fp.insjoaquimmir.cat>
2024-05-09 19:56:15 +0200

    Login/Register and basic estrucutre components upgrade

--- react/src/components/auth/LoginRegister.jsx
+++ react/src/components/auth/LoginRegister.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
-import { Login } from './Login';
-import { Register } from './Register';
+import Login from './Login';
+import Register from './Register';
 
 const LoginRegister = () => {
 
