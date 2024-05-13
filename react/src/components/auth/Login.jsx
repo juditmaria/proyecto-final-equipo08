fafb28088d659ecf5ecbe141bd41ef9ee960bf54
juditmaria <jumoar@fp.insjoaquimmir.cat>
2024-05-13 19:06:24 +0200

    Savepoint login/register

--- react/src/components/auth/Login.jsx
+++ react/src/components/auth/Login.jsx
@@ -49,26 +49,27 @@ const Login = ({ setLogin }) => {
 
   return (
     <>
-      <header>Login</header>
-      <Form className='p-15'>
+      <header>Inicio de sessión</header>
+      <Form className='p-3'>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control required type="email" placeholder="Correo electrónico" />
           <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
+            Mínimo 8 cáracteres
           </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control required type="password" placeholder="Contraseña" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+       {/*  <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
+        </Form.Group> */}
         <Button variant="primary" type="submit">
           Submit
         </Button>
+        <Form.Text>
+            Mínimo 8 cáracteres
+          </Form.Text>
       </Form>
     
     
