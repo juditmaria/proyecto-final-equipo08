e08e07820590f01ff2beadd68630fd453e6865bc
juditmaria <jumoar@fp.insjoaquimmir.cat>
2024-06-14 12:44:46 +0200

    Hotfix room show

--- react/src/components/app/User/Promoter/RoomsPromoterShow.jsx
+++ react/src/components/app/User/Promoter/RoomsPromoterShow.jsx
@@ -3,96 +3,97 @@ import { URL_API } from '../../../../constants';
 import { useParams, Link } from 'react-router-dom';
 import { Container, Row, Col, Button, Spinner, Alert, ListGroup } from 'react-bootstrap';
 
+
 const ShowRoom = () => {
-    const { id } = useParams();
-    const [room, setRoom] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+   const { id } = useParams();
+   const [room, setRoom] = useState(null);
+   const [loading, setLoading] = useState(true);
+   const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchRoom = async () => {
-            try {
-                const response = await fetch(`${URL_API}rooms/${id}`);
-                if (response.ok) {
-                    const data = await response.json();
-                    setRoom(data.data);
-                } else {
-                    setError('Error fetching room');
-                }
-            } catch (error) {
-                setError('Error fetching room');
-            } finally {
-                setLoading(false);
-            }
-        };
 
-        fetchRoom();
-    }, [id]);
+   useEffect(() => {
+       const fetchRoom = async () => {
+           try {
+               const response = await fetch(`${URL_API}rooms/${id}`);
+               if (response.ok) {
+                   const data = await response.json();
+                   setRoom(data.data);
+               } else {
+                   setError('Error fetching room');
+               }
+           } catch (error) {
+               setError('Error fetching room');
+           } finally {
+               setLoading(false);
+           }
+       };
 
-    const handleDeleteRoom = async () => {
-        const confirmDelete = window.confirm('Are you sure you want to delete this room?');
-        if (confirmDelete) {
-            try {
-                const response = await fetch(`${URL_API}rooms/${id}`, {
-                    method: 'DELETE'
-                });
-                if (response.ok) {
-                    window.location.href = '/rooms-promoter';
-                } else {
-                    setError('Error deleting room');
-                }
-            } catch (error) {
-                setError('Error deleting room');
-            }
-        }
-    };
 
-    if (loading) return (
-        <Container className="text-center my-5">
-            <Spinner animation="border" />
-            <div>Loading...</div>
-        </Container>
-    );
-    if (error) return (
-        <Container className="text-center my-5">
-            <Alert variant="danger">{error}</Alert>
-        </Container>
-    );
-    if (!room) return (
-        <Container className="text-center my-5">
-            <Alert variant="warning">Room not found</Alert>
-        </Container>
-    );
+       fetchRoom();
+   }, [id]);
 
-    return (
-        <Container className="my-5">
-            <Row className="justify-content-center">
-                <Col md={8} lg={6}>
-                    <h2 className="mb-4 text-center">{room.name}</h2>
-                    <ListGroup variant="flush" style={{ margin: '0 auto', maxWidth: '50%' }}>
-                        <ListGroup.Item><strong>Capacity:</strong> {room.capacity}</ListGroup.Item>
-                        <ListGroup.Item><strong>Lines:</strong> {room.num_line}</ListGroup.Item>
-                        <ListGroup.Item><strong>Seats:</strong> {room.num_seat}</ListGroup.Item>
-                        {/* Agrega aquí los datos adicionales como película, sala, ubicación, fecha y hora */}
-                        {/* Por ejemplo, si estos datos están presentes en el objeto `room` */}
-                        <ListGroup.Item><strong>Movie:</strong> {room.movie}</ListGroup.Item>
-                        <ListGroup.Item><strong>Room:</strong> {room.room}</ListGroup.Item>
-                        <ListGroup.Item><strong>Location:</strong> {room.location}</ListGroup.Item>
-                        <ListGroup.Item><strong>Date:</strong> {room.date}</ListGroup.Item>
-                        <ListGroup.Item><strong>Time:</strong> {room.time}</ListGroup.Item>
-                    </ListGroup>
-                    <Button variant="danger m-4" onClick={handleDeleteRoom}>
-                        Delete
-                    </Button>
-                    <Link to="/rooms-promoter">
-                        <Button variant="secondary">
-                            Go Back to Rooms Promoter
-                        </Button>
-                    </Link>
-                </Col>
-            </Row>
-        </Container>
-    );
+
+   const handleDeleteRoom = async () => {
+       const confirmDelete = window.confirm('Are you sure you want to delete this room?');
+       if (confirmDelete) {
+           try {
+               const response = await fetch(`${URL_API}rooms/${id}`, {
+                   method: 'DELETE'
+               });
+               if (response.ok) {
+                   window.location.href = '/rooms-promoter';
+               } else {
+                   setError('Error deleting room');
+               }
+           } catch (error) {
+               setError('Error deleting room');
+           }
+       }
+   };
+
+
+   if (loading) return (
+       <Container className="text-center my-5">
+           <Spinner animation="border" />
+           <div>Loading...</div>
+       </Container>
+   );
+   if (error) return (
+       <Container className="text-center my-5">
+           <Alert variant="danger">{error}</Alert>
+       </Container>
+   );
+   if (!room) return (
+       <Container className="text-center my-5">
+           <Alert variant="warning">Room not found</Alert>
+       </Container>
+   );
+
+
+   return (
+       <Container className="my-5">
+           <Row className="justify-content-center">
+               <Col md={8} lg={6}>
+                   <h2 className="mb-4 text-center">{room.name}</h2>
+                   <ListGroup variant="flush" style={{ margin: '0 auto', maxWidth: '50%' }}>
+                       <ListGroup.Item><strong>Capacity:</strong> {room.capacity}</ListGroup.Item>
+                       <ListGroup.Item><strong>Lines:</strong> {room.num_line}</ListGroup.Item>
+                       <ListGroup.Item><strong>Seats:</strong> {room.num_seat}</ListGroup.Item>
+                       <ListGroup.Item><strong>Hour:</strong> {room.hour}</ListGroup.Item>
+                   </ListGroup>
+                   <Button variant="danger m-4" onClick={handleDeleteRoom}>
+                       Delete
+                   </Button>
+                   <Link to="/rooms-promoter">
+                       <Button variant="secondary">
+                           Go Back to Rooms Promoter
+                       </Button>
+                   </Link>
+               </Col>
+           </Row>
+       </Container>
+   );
 };
+
 
 export default ShowRoom;
